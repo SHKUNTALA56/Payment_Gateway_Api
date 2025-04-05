374ba446b5fe63dfe3f7cb8336e3ce79121fea8e
SHKUNTALA <briandaviskihiu@gmail.com>
2025-04-05 13:45:52 +0300

    Documentation Autocreation & static files debug

--- payment_gateway_api/settings.py
+++ payment_gateway_api/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#During deployment(static)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- payment_gateway_api/urls.py
+++ payment_gateway_api/urls.py
@@ -4,16 +4,36 @@ from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from payments import views  # Correct import
 from payments.views import HomePageView 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
-# Redirect root URL to HomePageView
+
+
+# Create a schema view with the permission set to allow any user to access
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Payment Gateway API",
+        default_version='v1',
+        description="API documentation for the payment gateway service",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@paymentgateway.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+          # Redirect root URL to HomePageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('payments.urls')),  # Include app's urls.py
 
+          #  View Documentation
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'), 
+
 
      # Registration & Login page   
     path('api/auth/register/', views.register_user, name='register-api'),  # Add this line
-
 
 
     # Root path for the home page
