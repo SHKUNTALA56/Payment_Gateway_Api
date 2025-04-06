7fdcbe46e920dc2d5f7faf99ed1d33d550a5e199
Brian Davis Kihiu <100779766+SHKUNTALA56@users.noreply.github.com>
2025-04-06 16:26:01 +0300

    Update settings.py

--- payment_gateway_api/settings.py
+++ payment_gateway_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-h-s5()2mq+cgscg_3lqq4i1lb#@(+@fu)2*e&p47i58ay#(x9b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
